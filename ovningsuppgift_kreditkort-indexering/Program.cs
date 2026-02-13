ca7107ec97eabd68817b511c12dcd3dd41aa9167
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2026-02-13 09:43:05 +0100

    Implement main menu for credit card operations

--- ovningsuppgift_kreditkort-indexering/Program.cs
+++ ovningsuppgift_kreditkort-indexering/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("1. Generate credit mock data");
+Console.WriteLine("2. List people");
+Console.WriteLine("3. List cards");
+Console.WriteLine("4. List transactions");
+Console.Write("\n\nEnter your choice: ");
+
+var choice = Console.ReadLine();
+
+switch (choice)
+{
+    case "1":
+        Console.WriteLine("Generating mock data...");
+        break;
+    case "2":
+        Console.WriteLine("Listing people...");
+        break;
+    case "3":
+        Console.WriteLine("Listing cards...");
+        break;
+    case "4":
+        Console.WriteLine("Listing transactions...");
+        break;
+    default:
+        Console.WriteLine("Invalid choice.");
+        break;
+}
